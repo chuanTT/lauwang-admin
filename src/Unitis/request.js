d416a2fd77bde555689e0f3f93625ae17c5d4f55
chuanTT <95389918+chuanTT@users.noreply.github.com>
2022-11-22 18:26:08 +0700

    Update request.js

--- src/Unitis/request.js
+++ src/Unitis/request.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const request = axios.create({
-  baseURL: "http://localhost:3020/api/",
+  baseURL: "https://lauwang-api.onrender.com/api/",
   headers: {
     'Access-Control-Allow-Origin': '*',
     'Content-Type': 'application/json',
